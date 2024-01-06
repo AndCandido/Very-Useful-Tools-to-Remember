b435fa58e979cf4f7bcf3fc70070f4dce1a3afa7
André <100149329+AndCandido@users.noreply.github.com>
2024-01-06 18:30:13 -0300

    Init

---
+++ src/main/java/io/github/AndCandido/Very/Useful/Tools/to/Remember/VeryUsefulToolsToRememberApplication.java
@@ -0,0 +1,13 @@
+package io.github.AndCandido.Very.Useful.Tools.to.Remember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeryUsefulToolsToRememberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeryUsefulToolsToRememberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/AndCandido/Very/Useful/Tools/to/Remember/VeryUsefulToolsToRememberApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.AndCandido.Very.Useful.Tools.to.Remember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeryUsefulToolsToRememberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
